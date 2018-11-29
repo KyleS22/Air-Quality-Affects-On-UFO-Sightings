4aaaa04daeada3f9d7a7eae36d7ca7fb2ff3b9c4
kyles22 <25314187+KyleS22@users.noreply.github.com>
2018-11-29 10:09:19 -0600

    The map vis seems to be working, still need better location data.  Should be ready for click events on the cities to generate the line ploy

--- combine_all_cities_and_sightings_locations.py
+++ combine_all_cities_and_sightings_locations.py
@@ -1,43 +1,44 @@
 import csv
+from tqdm import tqdm
 
 US_CITIES_FILE = "all_us_cities.csv"
-cached_city_locations = {}
+SIGHTINGS_FILE = ""
+cached_city_locations = []
 
 print("Processing City Locations:")
-    with open(US_CITIES_FILE) as cities_csv:
 
-        city_reader = csv.reader(cities_csv)
-        city_lines = [line for line in cities_csv]
-        city_lines = city_lines[1:]
+with open(US_CITIES_FILE) as cities_csv:
 
-        for city in tqdm(csv.reader(city_lines), total=len(city_lines)):
-            city_name = city[1]
-            lat = float(city[5])
-            longitude = float(city[6])
-            state = city[3]
+    city_reader = csv.reader(cities_csv)
+    city_lines = [line for line in cities_csv]
+    city_lines = city_lines[1:]
 
-            new_key = city_name.lower() + "_" + state.lower()
+    for city in tqdm(csv.reader(city_lines), total=len(city_lines)):
+        city_name = city[1]
+        state = city[3]
 
-            cached_city_locations[new_key] = {}
-            cached_city_locations[new_key]["longitude"] = longitude
-            cached_city_locations[new_key]["latitude"] = lat
+        new_key = city_name.lower() + "_" + state.lower()
+
+        cached_city_locations.append(new_key)
 
 
-print("Processing City Locations:")
-    with open(SIGHTINGS_FILE) as cities_csv:
+print("\nProcessing City Locations:")
+with open(SIGHTINGS_FILE) as cities_csv:
 
-        city_reader = csv.reader(cities_csv)
-        city_lines = [line for line in cities_csv]
-        city_lines = city_lines[1:]
+    city_reader = csv.reader(cities_csv)
+    city_lines = [line for line in cities_csv]
+    city_lines = city_lines[1:]
 
-        for city in tqdm(csv.reader(city_lines), total=len(city_lines)):
-            city_name = city[1]
-            lat = float(city[9])
-            longitude = float(city[10])
-            state = city[2]
+    for city in tqdm(csv.reader(city_lines), total=len(city_lines)):
+        city_name = city[1]
+        lat = float(city[9])
+        longitude = float(city[10])
+        state = city[2]
 
-            new_key = city_name.lower() + "_" + state.lower()
+        new_key = city_name.lower() + "_" + state.lower()
 
-            if not new_key in cached_city_locations:
-                with open('document.csv','a') as fd:
-                    fd.write("us",city_name,city_name,state,0,lat,longitude)
+        if not new_key in cached_city_locations:
+            with open(US_CITIES_FILE,'a') as fd:
+                fd.write("us",city_name,city_name,state,0,lat,longitude)
+
+            cached_city_locations.append(new_key)
